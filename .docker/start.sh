6d9f2fead9a8e2c85cf7ba49711ec8f5cb8f918e
andresrom1 <33395068+andresrom1@users.noreply.github.com>
2025-11-15 00:59:10 -0300

    migrate on container start

--- .docker/start.sh
+++ .docker/start.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-supervisord -c /etc/supervisor/conf.d/supervisord.conf
+set -e
+
+# Migraciones (runtime, con DB disponible)
+php artisan migrate --force
+
+# Cachear solo una vez (opcional pero recomendado)
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+# Lanzar supervisord (que ya arranca nginx + php-fpm)
+exec supervisord -c /etc/supervisor/conf.d/supervisord.conf
